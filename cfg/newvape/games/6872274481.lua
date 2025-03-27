d296ec0791ebfc1c8acd1805208654ab587b654c
vlctyy <kharispowell@hotmail.com>
2025-03-26 19:31:33 -0500

    Add files via upload

---
+++ cfg/newvape/games/6872274481.lua
@@ -0,0 +1,2 @@
+--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b3722c8ee2036a4c577f470856e34ec6.lua"))()
